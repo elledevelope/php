2a0a6dfd16b6354e589fd7c8517f9d40cba4b7ee
Elmira PETIT <elledevelope@gmail.com>
2023-10-04 10:26:42 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+include('./header.php');
+
+
+echo "<h1>CEPPIC PHP</h1>";
+?>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur officiis nostrum architecto rerum cum! Reprehenderit, dignissimos corporis. Reiciendis necessitatibus facere quaerat, optio excepturi laudantium provident itaque, voluptate dicta suscipit architecto?</p>
+
+<?php
+echo "Encore CEPPIC PHP";
+
+include('./footer.php');
+
+?>
