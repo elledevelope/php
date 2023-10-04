9e4ff143fc12608f94cb6185f5a02acddd80496b
Elmira PETIT <elledevelope@gmail.com>
2023-10-04 15:54:14 +0200

    Added operateurs

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
+
+
+// phpinfo(); // gives info about yout php version 
+
 require './functions.php';
 
 $titre = 'Operateurs PHP';
 
 require './index.view.php';
+
+$x = 4;
+$y = 2;
+$z = 5;
+
+//---------------------------OPERATEURS :
+/* 
+
++
+-
+*
+/
+**
+%
+
+
+ */
 
 
 ?>
