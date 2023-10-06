9595714286bc36ac817f30e2f1dc88fdd3c2e18b
Elmira PETIT <elledevelope@gmail.com>
2023-10-06 16:21:31 +0200

    Two different syntaxes of condition if else

--- index.php
+++ index.php
@@ -25,14 +25,21 @@ $user = [
     'competance' => ['php', 'react', 'python']
 ];
 
-$age = 10;
+// ---Syntaxe_1:
+/* $age = 10;
 if($age >= 18) {
     echo 'Peut voter';
-};
-// Another syntaxe:
+}
+else {
+        echo 'Ne peut pas voter!';
+    }; */
+    
+// ---Syntaxe_2:
 $age = 10;
-if($age >= 18) :
+if ($age >= 18) :
     echo 'Peut voter';
+else :
+    echo 'Ne peut pas voter!';
 endif;
 
 
