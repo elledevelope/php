fe961209366f6855b97c2cc2aaf9233d234bbfa5
Elmira PETIT <elledevelope@gmail.com>
2023-10-12 13:55:08 +0200

    Added $_SERVER and $GLOBALS)

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php require './functions.php';
 $titre = "Formulaire php";
 
+// dd($GLOBALS); //$GLOBALS — Référence toutes les variables disponibles dans un contexte global
+//dd($_SERVER);  //$_SERVER — Variables de serveur et d'exécution
 
-// dbug($_GET)
-// dbug($_POST)
+// dbug($_GET) //this is Superglobal
+// dbug($_POST) //this is Superglobal
 ?>
 
-
+<!-----------------------------------------Formulaire PHP: -------------------------------------->
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -20,14 +22,18 @@ $titre = "Formulaire php";
 <body>
     <h1>Formulaire</h1>
     <form action="./traitment_php.php" method="post"> <!-- to use with dbug($_POST) -->
-        <label for="nom">Nom:
-        <input type="text" name="nom" required>
+        <label for="name">Name:
+        <input type="text" name="name" required placeholder="Your Name">
         </label>
 
         <label for="email">Email:
         <input type="email" name="email">
         </label>
-        
+
+        <label for="comment">Comment :
+        <textarea name="comment" id = "comment" cols = "30" rows ="10"></textarea>
+        </label>
+
         <input type="submit" value="Valider">
     </form>
 </body>
