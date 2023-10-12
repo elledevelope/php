f10d74025b07901bb4f57ae64be6a90fa2374c21
Elmira PETIT <elledevelope@gmail.com>
2023-10-12 11:42:43 +0200

    Typage de fontions

--- functions.php
+++ functions.php
@@ -1,17 +1,18 @@
 <?php
 
-function dbug($value) {
-echo '<pre style="background-color:pink;color:white;overflow:auto;paddind:10px;">';
-print_r($value);
-// var_dump($value);
-echo '</pre>';
-};
+declare(strict_types=1); //---------Typage de fontions forcée 12.10.23
 
+// function dbug($value) {
+// echo '<pre style="background-color:pink;color:white;overflow:auto;paddind:10px;">';
+// print_r($value);
+// // var_dump($value);
+// echo '</pre>';
+// };
 
-function dd($value) {
-dbug($value);
-// die('Script php arrété !!!');
-};
+// function dd($value) {
+// dbug($value);
+// // die('Script php arrété !!!');
+// };
 
 
 //________________________________________________________________________________12.10.2023________________________________:
@@ -30,22 +31,46 @@ dbug($value);
 } ;
 echo calcul(15, 15);
  */
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Créer une foction qui calcul le prix TCC en lui passant un prix HT et la TVA:
-function calculPrixTTC($prixHT, $tva)
+/* function calculPrixTTC($prixHT, $tva)
 {
     return $prixHT + ($tva * $prixHT) / 100;
 };
 echo calculPrixTTC(34,10);
-
+ */
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Créer une foction qui calcil l'age d'une personne en lui donnant sa date de naissance:
-function calculAge($anneeNaissance)
+//Créer une foction qui calcul l'age d'une personne en lui donnant sa date de naissance:
+/* function calculAge($anneeNaissance)
 {
     $anneeEnCour = date("Y");
     return $anneeEnCour - $anneeNaissance;
 };
-echo calculAge(1990);
+echo calculAge(1990); */
+
+//////////////------------------------------------------------------Function anonyme:
+/* $calcul = function ($nb) {
+    return $nb + 1;
+};
+echo $calcul(25); */
+
+
+//////////////------------------------------------------------------ Typage de fontions --int(number entier), float (decimail)-- (avant au debut de la page il faut mettre: declare(strict_types=1); ):
+
+/* function calcul(int $a , $b)
+{
+    return $a + $b;
+} ;
+echo calcul("2", 5); //echo will give ERROR because int $a forces $a to be a number and "2" is not a number  */
+
+///////
+function calcul(float  $a , int | float $b) //  pipe symbol |  means "or"
+{
+    return $a + $b;
+} ;
+echo calcul(2.5, 5.5); 
 
 
 
