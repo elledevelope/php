c05142ef4ba53c4fe7c9b5b9c3e6d21432796f11
Elmira PETIT <elledevelope@gmail.com>
2023-10-12 11:58:32 +0200

    Added the `global` keyword

--- functions.php
+++ functions.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1); //---------Typage de fontions forcée 12.10.23
 
-// function dbug($value) {
-// echo '<pre style="background-color:pink;color:white;overflow:auto;paddind:10px;">';
-// print_r($value);
-// // var_dump($value);
-// echo '</pre>';
-// };
+function dbug($value) {
+echo '<pre style="background-color:pink;color:white;overflow:auto;paddind:10px;">';
+print_r($value);
+// var_dump($value);
+echo '</pre>';
+};
 
-// function dd($value) {
-// dbug($value);
-// // die('Script php arrété !!!');
-// };
+function dd($value) {
+dbug($value);
+// die('Script php arrété !!!');
+};
 
 
 //________________________________________________________________________________12.10.2023________________________________:
@@ -66,12 +66,22 @@ echo $calcul(25); */
 echo calcul("2", 5); //echo will give ERROR because int $a forces $a to be a number and "2" is not a number  */
 
 ///////
-function calcul(float  $a , int | float $b) //  pipe symbol |  means "or"
+/* function calcul(float  $a , int | float $b) //  pipe symbol |  means "or"
 {
     return $a + $b;
 } ;
-echo calcul(2.5, 5.5); 
+echo calcul(2.5, 5.5);  */
 
+////////////////------------------------------------the `global` keyword:
+$x = 5;
+$y = 3;
+function calcul($xPasseEnParametre) {
+    // global $y; //the global keyword is used to declare a variable as a global variable within the current scope. When a variable is declared as global, it can be accessed and modified from both the current scope and any nested function or code blocks within that scope.
+    $y = 7;
+    return $xPasseEnParametre + $y;
+};
+echo calcul(5);
+dd($y);
 
 
 ?>
