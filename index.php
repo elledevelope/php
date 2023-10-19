56f8b0194275cabc085e42fce5c8a245d84b1e86
Elmira PETIT <elledevelope@gmail.com>
2023-10-19 15:25:58 +0200

    Created architecture

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
-require './functions.php';
+// require './fonctions.php';
 
+// dd($_SERVER);
 // dd($_SERVER['REQUEST_URI']);
-
-
-//parse_url — Analyse une URL et retourne ses composants  https://www.php.net/manual/fr/function.parse-url.php :
 // dd(parse_url($_SERVER['REQUEST_URI'])['path']);
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-dd($uri);
 
-if ($uri === '/php/') :
-    header('./php/index.php');
-    die();
-    echo 'Je suis à la racine de mon dossier de travail ';
-elseif ($uri == '/php/contact') :
-echo 'contact.php';
-endif;
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/php' => 'contolleurs/index.php',
+    '/contact' => 'contolleurs/contact.php',
+    '/notes' => 'contolleurs/notes.php',
+];
