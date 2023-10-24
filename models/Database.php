a5703b26903030125b23606c6e47d2cd22e78495
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 14:20:05 +0200

    Added comments

--- models/Database.php
+++ models/Database.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$pass = '';
+$user = 'root'; //Database server - User: root@localhost 
+$pass = ''; //password of MySQL server (we don't have password)
 
-$connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass);
+$connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass); //connection PHP to server MySQL via class PDO
 
 
 
