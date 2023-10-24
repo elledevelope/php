b1224a0a4715a247912de7776bc2b948de59cb54
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 11:40:47 +0200

    Added $urlToControllers

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
-// require './fonctions.php';
+require './functions.php';
 
 // dd($_SERVER);
 // dd($_SERVER['REQUEST_URI']);
 // dd(parse_url($_SERVER['REQUEST_URI'])['path']);
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$uriPath = parse_url($_SERVER['REQUEST_URI'])['path'];
+//dd($uri);
 
 $routes = [
-    '/php' => 'contolleurs/index.php',
-    '/contact' => 'contolleurs/contact.php',
-    '/notes' => 'contolleurs/notes.php',
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    '/notes' => 'controllers/notes.php',
 ];
+// dbug($routes);
+
+$urlToControllers = match($uriPath) {
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+};
+// dbug($urlToControllers);
+
+require $urlToControllers;
+exit();
