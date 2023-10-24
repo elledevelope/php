ccaf709a7b300264a1880ff8a982c0776309b968
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 15:23:22 +0200

    Access to notes

--- models/Database.php
+++ models/Database.php
@@ -5,6 +5,12 @@ $pass = ''; //password of MySQL server (we don't have password)
 
 $connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass); //connection PHP to server MySQL via class PDO
 
-dd($connexion);
+// dd($connexion); //checking than connextion to Database MySQL works
+
+$notes = $connexion->query('SELECT * FROM note')->fetchAll(PDO::FETCH_ASSOC); //query is requette, fetchAll is to get all info from table note
+//fetchAll(PDO::FETCH_ASSOC) is to make array associative 
+// dd($notes);
+
+include 'views/notes.view.php';
 
 ?>
