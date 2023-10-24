e891d0cf29fc6793b1fde680c21e0792b06298d4
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 15:32:27 +0200

    Created note.php

---
+++ controllers/note.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'models/Database.php';
+
+$notes = $connexion->query('SELECT * FROM note')->fetchAll(PDO::FETCH_ASSOC); //query is requette, fetchAll is to get all info from table note
+//fetchAll(PDO::FETCH_ASSOC) is to make array associative 
+// dd($notes);
+
+
+require 'views/notes.view.php';
+
+?>
