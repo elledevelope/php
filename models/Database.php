c4ec71d96c731d202f05edafbe90ceed486f9380
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 15:27:48 +0200

    Cut out $notes = $connexion to notes.php

--- models/Database.php
+++ models/Database.php
@@ -7,9 +7,7 @@ $connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass); //conne
 
 // dd($connexion); //checking than connextion to Database MySQL works
 
-$notes = $connexion->query('SELECT * FROM note')->fetchAll(PDO::FETCH_ASSOC); //query is requette, fetchAll is to get all info from table note
-//fetchAll(PDO::FETCH_ASSOC) is to make array associative 
-// dd($notes);
+
 
 include 'views/notes.view.php';
 
