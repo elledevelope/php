903ed4f45fa796d0f3f10358e28cbe1975cb330c
Elmira PETIT <elledevelope@gmail.com>
2023-10-24 14:21:58 +0200

    Linked function.php  to Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,9 +1,12 @@
 <?php
+
+include 'config/functions.php';
+
 $user = 'root'; //Database server - User: root@localhost 
 $pass = ''; //password of MySQL server (we don't have password)
 
 $connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass); //connection PHP to server MySQL via class PDO
 
-
+dd($connexion);
 
 ?>
