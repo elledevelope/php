31aea0dbd811939c49462375c75b866ae1bff4cc
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 14:35:07 +0200

    Update

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
 </head>
 
 <body>
-    <h1>Notes</h1>
-    <ul>
+    <h1>Welcome to Notes</h1>
+    <ul id="nav" >
         <li><a href="/">Acceuil</a></li>
         <li><a href="/notes">Notes</a></li>
         <li><a href="/contact">Contact</a></li>
