7ae2a1096fb4476069ade73ade1ab25e445b613f
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 13:48:03 +0200

    Update

--- controllers/note.php
+++ controllers/note.php
@@ -4,6 +4,16 @@ require 'models/Database.php';
 
 
 // dd($_GET);
+/* 
+ if(!isset($_GET['id']) || !is_numeric($_GET['id'])) :
+    abort();
+endif;
+
+if (empty($note) )  :
+    abort();
+endif;  */
+
+
 $id = $_GET['id'];
 
 $note = $connexion->prepare('SELECT * FROM `note` AS n
@@ -15,10 +25,11 @@ $note->bindParam(':id',$id);
 $note->execute();
 $note = $note->fetch(PDO::FETCH_ASSOC);
 // dd($note);
+ 
 
 
-
-/* $id = $_GET['id'];
+/* 
+$id = $_GET['id'];
 
 $query = 'SELECT note.id, note.titre, note.content, note.created_at, user.name
           FROM note
@@ -29,10 +40,7 @@ $note = $connexion->prepare($query); //The prepare() method is used to create a 
 $note->bindParam(':id', $id);
 $note->execute();
 $note = $note->fetch(PDO::FETCH_ASSOC); //method is called on the prepared statement to retrieve one row of data from the result set in the form of an associative array. The PDO::FETCH_ASSOC option specifies that you want the data organized in the form of an associative array, where column names are associated with their values.
- */
-
-
-
+  */
 
 require 'views/note.view.php';
 
