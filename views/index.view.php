29a6ce5de0903d2d6e0e1464bbfd6e09ceae54bd
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 10:30:46 +0200

    Separeted index.view on header.php and footer.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Index</h1>
-</body>
-</html>
+<?php require 'partials/header.php';  ?>
+
+
+    <h1>IndexMenu</h1>
+
+<?php require 'partials/header.php';  ?>
+
+   
