7c62720a7f7037411974bb013fe7f93b64b7309e
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 11:37:54 +0200

    Added link to note-delete

--- views/note.view.php
+++ views/note.view.php
@@ -4,6 +4,9 @@
 <h2><?=$note['titre']?></h2>
 <p><?=$note['content']?></p>
 <p>Publiée le <?=$note['created_at']?> par <strong><?= $note['name']?></strong></p>
+
+<p><a href="/note-delete?id=<?=$note['id']?>">Supprimer cette note</a></p>
+
 <a href="/notes">Retour à la liste des notes</a>
 
 <?php require 'partials/footer.php';  ?>
