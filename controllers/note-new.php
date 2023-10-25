d70c51279f70c49ccbcb2125ec67b24fb5012f32
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 16:49:33 +0200

    Added FILTER_SANITIZE

--- controllers/note-new.php
+++ controllers/note-new.php
@@ -4,9 +4,29 @@ require 'models/Database.php';
 
 $users = $connexion->query('SELECT * FROM user')->fetchAll(PDO::FETCH_ASSOC);
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') :
-    dd($_POST);
-endif;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // dd($_POST);
 
+$titre = filter_var($_POST['titre'],
+FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+$content = filter_var($_POST['contecnt'],
+FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+$user = filter_var($_POST['user'],
+FILTER_SANITIZE_NUMBER_INT);
+
+
+    $noteNew = $connexion->prepare('INSERT INTO note (titre, content, user_id)
+    VALUES (:titre, :content, :user_id)');
+
+    $noteNew->bindParam(':titre', $_POST['titre']);
+    $noteNew->bindParam(':content', $_POST['content']);
+    $noteNew->bindParam(':user_id', $_POST['user_id']);
+    $noteNew->execute();
+    
+    header('Location: /notes');
+    exit();
+}
 
 require 'views/note-new.view.php';
