47219244ea8fa29ffcaf0dc326930bec815c0e65
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 16:08:07 +0200

    Update

--- models/Database.php
+++ models/Database.php
@@ -3,7 +3,7 @@
 $user = 'root'; //Database server - User: root@localhost 
 $pass = ''; //password of MySQL server (we don't have password)
 
-$connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass); //connection PHP to server MySQL via class PDO
+$connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass, [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);  //connection PHP to server MySQL via class PDO
 
 // dd($connexion); //checking than connextion to Database MySQL works
 
