42939514d2e8fa23c979e6d7012ff867f0f33327
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 16:07:57 +0200

    Added request method POST

--- controllers/note-new.php
+++ controllers/note-new.php
@@ -4,5 +4,9 @@ require 'models/Database.php';
 
 $users = $connexion->query('SELECT * FROM user')->fetchAll(PDO::FETCH_ASSOC);
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') :
+    dd($_POST);
+endif;
+
 
 require 'views/note-new.view.php';
--- views/note-new.view.php
+++ views/note-new.view.php
@@ -7,7 +7,7 @@
 
 
 
-<form method="POST" action="">
+<form method="POST">
     <label for="titre">Titre :</label>
     <input type="text" name="titre" id="titre">
 
@@ -16,11 +16,13 @@
 
     <label for="user">Auteur :</label>
     <select name="user" id="user">
+
         <option value="" selected></option>
 
         <?php foreach ($users as $user) { ?>
             <option value="<?= $user['user_id'] ?>"><?= $user['name'] ?></option>
-        <?php } ?>
+        <?php }; ?>
+
     </select>
 
 
