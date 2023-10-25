91e82ba42ce61e8bac2d34066a4fd3d7e577a00b
Elmira PETIT <elledevelope@gmail.com>
2023-10-25 14:46:02 +0200

    Added form

--- views/note-new.view.php
+++ views/note-new.view.php
@@ -2,5 +2,14 @@
 
 <h2>Ajout d'une nouvelle note</h2>
 
+<form action="">
+    <label for="titre">Titre :</label>
+    <input type="text" name="titre" id="titre">
+
+    <label for="content">Contenu :</label>
+    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    
+    <input type="submit" value="Ajouter">
+</form>
 
 <?php require 'partials/footer.php';  ?>
