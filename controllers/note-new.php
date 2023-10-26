14c243c17846ca21ee1e14d047fbd3109103406a
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 10:13:35 +0200

    Modified comment

--- controllers/note-new.php
+++ controllers/note-new.php
@@ -7,21 +7,25 @@ $users = $connexion->query('SELECT * FROM user')->fetchAll(PDO::FETCH_ASSOC);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // dd($_POST);
 
-$titre = filter_var($_POST['titre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $titre = filter_var($_POST['titre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-$content = filter_var($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $content = filter_var($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-$user = filter_var($_POST['user'], FILTER_SANITIZE_NUMBER_INT);
+    $user = filter_var($_POST['user'], FILTER_SANITIZE_NUMBER_INT);
 
 
-//Limit longuer of a titre of a note:
-if(strlen($titre) >= 100 ) :
-dd('Titre trop loooooooooooooooooog!!!!!');
-endif;
-
-if(strlen($content) >= 1000 ) :
-    dd('Text trop loooooooooooooooooog!!!!!');
+    //Limit number of symbols in titre and content:
+    if (strlen($titre) >= 100) :
+        dd('Titre trop loooooooooooooooooog!!!!!');
     endif;
+
+    if (strlen($content) >= 1000) :
+        dd('Text trop loooooooooooooooooog!!!!!');
+    endif;
+
+
+
+
 
 
     $noteNew = $connexion->prepare('INSERT INTO note (titre, content, user_id)
@@ -31,7 +35,7 @@ if(strlen($content) >= 1000 ) :
     $noteNew->bindParam(':content', $_POST['content']);
     $noteNew->bindParam(':user_id', $_POST['user_id']);
     $noteNew->execute();
-    
+
     header('Location: /notes');
     exit();
 };
