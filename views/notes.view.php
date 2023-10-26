b557b73b30b563d3a67ae0ac8329202128c1766b
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 09:45:08 +0200

    Moved ul inside a loop to style it later

--- views/notes.view.php
+++ views/notes.view.php
@@ -1,26 +1,22 @@
 <?php require 'partials/header.php';  ?>
 
 <h2>Notes :</h2>
-    <ul id="notes-view-ul">
-    <?php foreach ($notes as $note) : ?>
-    <li id="notes-view-li">
-        <a href="/note?id=<?=$note['id']?>">
-        <?=$note['titre']?>
-        </a> - <a href="/note-delete?id=<?=$note['id']?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">x</a>
-    </li>
 
-<?php 
+<?php foreach ($notes as $note) : ?>
+    <ul id="notes-view-ul">
+        <li id="notes-view-li">
+            <a href="/note?id=<?= $note['id'] ?>">
+                <?= $note['titre'] ?>
+            </a> - <a href="/note-delete?id=<?= $note['id'] ?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">x</a>
+        </li>
+    </ul>
+<?php
 endforeach;
 ?>
-    </ul>
 
-    <p id="ajout-note-btn">
-        <a href="/note-new">Ajouter une note</a>
-    </p>
+
+<p id="ajout-note-btn">
+    <a href="/note-new">Ajouter une note</a>
+</p>
 
 <?php require 'partials/footer.php';  ?>
-
-
-
-
-
