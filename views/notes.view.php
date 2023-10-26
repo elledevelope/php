2294ef1df544b615dfd3d851c290fd8d5674b0f7
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 10:04:51 +0200

    Removed last commit

--- views/notes.view.php
+++ views/notes.view.php
@@ -1,22 +1,26 @@
 <?php require 'partials/header.php';  ?>
 
 <h2>Notes :</h2>
-
-<?php foreach ($notes as $note) : ?>
     <ul id="notes-view-ul">
-        <li id="notes-view-li">
-            <a href="/note?id=<?= $note['id'] ?>">
-                <?= $note['titre'] ?>
-            </a> - <a href="/note-delete?id=<?= $note['id'] ?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">x</a>
-        </li>
-    </ul>
-<?php
+    <?php foreach ($notes as $note) : ?>
+    <li id="notes-view-li">
+        <a href="/note?id=<?=$note['id']?>">
+        <?=$note['titre']?>
+        </a> - <a href="/note-delete?id=<?=$note['id']?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">x</a>
+    </li>
+
+<?php 
 endforeach;
 ?>
+    </ul>
 
-
-<p id="ajout-note-btn">
-    <a href="/note-new">Ajouter une note</a>
-</p>
+    <p id="ajout-note-btn">
+        <a href="/note-new">Ajouter une note</a>
+    </p>
 
 <?php require 'partials/footer.php';  ?>
+
+
+
+
+
