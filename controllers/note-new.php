c5acfb6b23ea8d42123eb12c995c3d09928090ef
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 14:01:20 +0200

    Fixed issue user_id beeing NULL in form submit

--- controllers/note-new.php
+++ controllers/note-new.php
@@ -1,43 +1,63 @@
 <?php
-
 require 'models/Database.php';
 
-$users = $connexion->query('SELECT * FROM user')->fetchAll(PDO::FETCH_ASSOC);
+// Fetch user data
+$users = $connexion->query('SELECT user_id,name FROM user')->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // dd($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+    $errors = [];
+    //$errors = '';
 
-    // $errors = [];
-    $errors = '';
 
+    // Sanitize and validate user inputs
     $titre = trim(filter_var($_POST['titre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-
     $content = trim(filter_var($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-
     $user = trim(filter_var($_POST['user'], FILTER_SANITIZE_NUMBER_INT));
 
     // Limit number of symbols  & not allows 0 symbol in titre and content:
-/*     if (strlen($titre) >= 100 || strlen($titre) === 0) {
-        $errors[] = 'Titre trop long!!';
-    }
-    if (strlen($content) >= 1000) {
-        $errors[] = 'Text trop long!!';
-    }
- */
-    if (strlen($content) >= 1000 || strlen($titre) === 0 || strlen($titre) >= 100 || strlen($content) === 0) {
-        $errors = 'Le titre et le text sont incorrects!!';
-    };
+    if (strlen($titre) === 0) :
+        $errors[] = 'Titre vide !!!';
+    endif;
 
-    if (empty($errors)) {
-        $noteNew = $connexion->prepare('INSERT INTO note (titre, content, user_id) VALUES (:titre, :content, :user_id)');
-        $noteNew->bindParam(':titre', $_POST['titre']);
-        $noteNew->bindParam(':content', $_POST['content']);
-        $noteNew->bindParam(':user_id', $_POST['user_id']);
+    if (strlen($titre) >= 100) :
+        $errors[] = 'Titre trop long !!!';
+    endif;
+
+    if (strlen($content) === 0) :
+        $errors[] = 'Contenu vide !!!';
+    endif;
+
+    if (strlen($content) >= 1000) :
+        $errors[] = 'Contenu supérieur à 1000 caratéres !!!';
+    endif;
+
+    /*     if (strlen($titre) >= 100 || strlen($titre) === 0) :
+        $errors[] = 'Titre trop long !!!';
+    endif; */
+
+    /*     if (strlen($titre) >= 100 || strlen($titre) === 0) :
+        $errors[] = 'Titre trop long !!!';
+    endif;
+
+    if (strlen($content) >= 100 || strlen($content) === 0) :
+        $errors[] = 'Contenue trop long bouuuu !!!';
+    endif; */
+
+    /*     if (strlen($titre) >= 100 || strlen($titre) === 0 || strlen($content) >= 1000 || strlen($content) === 0) :
+        $errors = 'Le titre ou le contenue de votre note est incorrect !!!';
+    endif; */
+
+    if (empty($errors)) :
+        $noteNew = $connexion->prepare('INSERT INTO note (titre,content,user_id) VALUES (:titre , :content , :user_id)');
+        $noteNew->bindValue(':titre', $titre);
+        $noteNew->bindValue(':content', $content);
+        $noteNew->bindValue(':user_id', $user);
         $noteNew->execute();
 
         header('Location: /notes');
         exit();
-    }
-}
+    endif;
 
-require 'views/note-new.view.php';
+endif;
+
+include 'views/note-new.view.php';
