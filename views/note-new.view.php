1f8d3bd4508acc4a434a72198049e296c11941c4
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 15:08:38 +0200

    Update pre-fill input textarea

--- views/note-new.view.php
+++ views/note-new.view.php
@@ -9,11 +9,17 @@
 
 <form method="POST">
 <label for="titre">Titre :</label>
-<input type="text" name="titre" id="titre" value="<?php if(isset($_POST['titre'])) { echo $_POST['titre']; } ?>">
- <!-- required -->
+<input type="text" name="titre" id="titre" value="<?= isset($_POST['titre']) ? $_POST['titre'] : '' ?>">
+<?php //if(isset($_POST['titre'])) { echo $_POST['titre']; } ?>
 
     <label for="content">Contenu :</label>
     <textarea name="content" id="content" cols="30" rows="10"><?= isset($_POST['content']) ? $_POST['content'] : '' ?></textarea> <!--- pre-fill an input field with a value that was previously submitted through a form --->
+    <?php //if(isset($_POST['content'])) : { echo $_POST['content']; } ?> 
+    <!---Explanation:     
+    --- When the form is initially displayed, the $content variable is set to an empty string because the form has not been submitted yet.
+    ---- If the form is submitted, the $content variable is updated with the value of $_POST['content'], which contains the user's content.
+    --- The <textarea> in the form is set to display the value of $content, which can be either the user's input if the form was submitted or an empty string if it's the initial form display.
+    --- This way, when the form is redisplayed (e.g., due to validation errors), the previously submitted content is shown in the <textarea>, allowing the user to edit or resubmit it without losing their input.  --->
    
 
     <label for="user">Auteur :</label>
