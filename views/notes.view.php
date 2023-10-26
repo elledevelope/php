d5712f5b79dcda6d176ec4f0055e8d81cc75942d
Elmira PETIT <elledevelope@gmail.com>
2023-10-26 09:17:50 +0200

    Update

--- views/notes.view.php
+++ views/notes.view.php
@@ -1,9 +1,9 @@
 <?php require 'partials/header.php';  ?>
 
-<h2>Notes</h2>
-    <ul>
+<h2>Notes :</h2>
+    <ul id="notes-view-ul">
     <?php foreach ($notes as $note) : ?>
-    <li>
+    <li id="notes-view-li">
         <a href="/note?id=<?=$note['id']?>">
         <?=$note['titre']?>
         </a> - <a href="/note-delete?id=<?=$note['id']?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">x</a>
