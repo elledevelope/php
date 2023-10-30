bf15721cab3c7cef0322bdbdc23732cfee99b43c
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 18:31:24 +0100

    Note.view added link to modifier note

--- views/note.view.php
+++ views/note.view.php
@@ -1,14 +1,14 @@
-<?php require 'partials/header.php';  ?>
+<?php require 'partials/header.php' ?>
 
-<h2>Note</h2>
 <h2><?= $note['titre'] ?></h2>
 <p><?= $note['content'] ?></p>
 <p>Publiée le <?= $note['created_at'] ?> par <strong><?= $note['name'] ?></strong></p>
 
-<p><a class="bnt" href="/note-delete?id=<?= $note['id'] ?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">Supprimer cette note</a></p>
+<p><a href="/note-update?id=<?=$note['id']?>" class="btn">Modifier cette article</a></p>
 
-<p><a href="/note-update" class="bnt">Modifier cette article</a></p>
+<p><a href="/note-delete?id=<?=$note['id']?>" onClick="return confirm('Etes vous certain de vouloir supprimer cette note !?');" class="btn error">
+Supprimer cette note
+</a></p>
 
-<a href="/notes">Retour à la liste des notes</a>
-
-<?php require 'partials/footer.php';  ?>
+<p><a href="/notes" class="btn">Retour à la liste des notes</a></p>
+<?php require 'partials/footer.php' ?>
