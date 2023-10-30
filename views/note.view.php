dc44390a5ed53cc32f01062e09216128322f7af3
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 16:08:58 +0100

    Created link to note-update in note.view

--- views/note.view.php
+++ views/note.view.php
@@ -5,8 +5,9 @@
 <p><?= $note['content'] ?></p>
 <p>Publiée le <?= $note['created_at'] ?> par <strong><?= $note['name'] ?></strong></p>
 
-<p><a href="/note-delete?id=<?= $note['id'] ?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">Supprimer cette note</a></p>
+<p><a class="bnt" href="/note-delete?id=<?= $note['id'] ?>" onClick="return confirm('Etes-vous certain de vouloir supprimer cette note?');">Supprimer cette note</a></p>
 
+<p><a href="/note-update" class="bnt">Modifier cette article</a></p>
 
 <a href="/notes">Retour à la liste des notes</a>
 
