0a79ae8442d3aacf7e36553be977489d31e59929
Elmira PETIT <elledevelope@gmail.com>
2023-10-30 14:02:41 +0100

    Added link to new-note in nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -4,6 +4,7 @@
 <ul id="nav" >
         <li><a href="/">Acceuil</a></li>
         <li><a href="/notes">Notes</a></li>
+        <li><a href="/note-new">Ajouter Note</a></li>
         <li><a href="/contact">Contact</a></li>
     </ul>
 
