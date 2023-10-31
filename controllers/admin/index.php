db400db59b4f8a655ec78f79ccb5a40ffd7aa598
Elmira PETIT <elledevelope@gmail.com>
2023-10-31 11:58:38 +0100

    Created index and index.view admin

---
+++ controllers/admin/index.php
@@ -0,0 +1,5 @@
+
+
+
+
+<?php require 'views/admin/index.view.php';  ?>
---
+++ views/admin/index.view.php
@@ -0,0 +1,5 @@
+<?php require 'partials/header.php' ?>
+
+<h2>admin Index.view.php</h2>
+
+<?php require 'partials/footer.php' ?>
