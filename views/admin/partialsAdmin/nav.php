3a22ede09f3ced4611d55db72878592cb3fb6b09
Elmira PETIT <elledevelope@gmail.com>
2023-10-31 16:17:24 +0100

    Update nav link to acceuil from admin/index

--- views/admin/partialsAdmin/nav.php
+++ views/admin/partialsAdmin/nav.php
@@ -3,6 +3,7 @@
         <div class="nav-wrapper">
             <a class="brand-logo" href="/admin">MyAdminNotes</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="/">Acceuil</a></li>
                 <li><a href="/dashborad">Dashborad</a></li>
                 <li><a href="/admin/users">Users list</a></li>
                 <li><a href="/user-new">Créer user</a></li>
