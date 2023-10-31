4d99fc17c35556b44a08cbdcaff4cc730cba20b3
Elmira PETIT <elledevelope@gmail.com>
2023-10-31 15:57:41 +0100

    Update nav link to Admin

--- views/admin/partialsAdmin/nav.php
+++ views/admin/partialsAdmin/nav.php
@@ -1,9 +1,8 @@
 <div class="header">
     <nav>
         <div class="nav-wrapper">
-            <a class="brand-logo" href="/">MyAdminNotes</a>
+            <a class="brand-logo" href="/admin">MyAdminNotes</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="/">Acceuil</a></li>
                 <li><a href="/dashborad">Dashborad</a></li>
                 <li><a href="/admin/users">Users list</a></li>
                 <li><a href="/user-new">Créer user</a></li>
