59a64ef62234709029e6555503376d0547d03c0c
Elmira PETIT <elledevelope@gmail.com>
2023-10-31 15:57:29 +0100

    Update nav link to Admin

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,7 @@
         <div class="nav-wrapper">
             <a class="brand-logo" href="/">LogoNotes</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="/">Acceuil</a></li>
+                <li><a href="/admin">Admin</a></li>
                 <li><a href="/notes">Notes</a></li>
                 <li><a href="/note-new">Ajouter Note</a></li>
                 <li><a href="/dashborad">Dashborad</a></li>
