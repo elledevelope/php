957ebb9028effe8d48ba34a63191db7970ec9086
Elmira PETIT <elledevelope@gmail.com>
2023-11-16 16:49:23 +0100

    Bug in requete

--- controllers/note/notes.php
+++ controllers/note/notes.php
@@ -1,10 +1,23 @@
 <?php
 
 require 'models/Database.php';
+if (isset($_SESSION['isLogged']) && isset($_SESSION['userId'])) :
 
-$notes = $connexion->query('SELECT * FROM note ORDER BY id DESC')->fetchAll(); //query is requette, fetchAll is to get all info from table note
-//fetchAll(PDO::FETCH_ASSOC) is to make array associative 
+    $notes = $connexion->query('SELECT * FROM note AS n
+INNER JOIN user AS u
+ON n.user_id = u.user_id
+WHERE u.status = 1 AND u.role = `role_user` AND u.user_id = :userid
+ORDER BY id DESC');
+    $notes->bindValue(':userid', $_SESSION['userId'], PDO::PARAM_STR);
+    $notes->bindValue(':password', $password, PDO::PARAM_STR);
+    $notes->execute();
+    $notes = $notes->fetchAll();
+else :
+    header('Location: /');
+    die();
+endif;
+
+// $notes = $connexion->query('SELECT * FROM note ORDER BY id DESC')->fetchAll(); 
 // dd($notes);
-
 
 require 'views/note/notes.view.php';
