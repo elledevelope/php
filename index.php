198d5a5ca70858f0552d05a7699baaff62cb1dcc
Elmira PETIT <elledevelope@gmail.com>
2023-11-16 10:06:14 +0100

    Added setcookie()

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-require 'config/functions.php';
-require 'core/router.php';
+//COOKIE
+$value = 'Valeur de test';
 
+// setcookie("TestCookie", $value);
+// setcookie("TestCookie", $value, time()+3600);  //3600sec = 1h
+setcookie("TestCookie", $value, time()-3600);  // negative time is used to delete cookie
+
+echo $_COOKIE["TestCookie"];
+
+
+//SESION
